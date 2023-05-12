81872bbfe1607bd7187b1460fbbd3fc6de63349a
JulianMiranda <jmirandauria@gmail.com>
2023-05-11 19:06:36 -0500

    Cambios

--- src/Terms.jsx
+++ src/Terms.jsx
@@ -22,16 +22,15 @@ export const Terms = () => {
 			<h4 style={{textAlign: 'left'}}>OBJETO DEL CONTRATO.</h4>
 			<p>
 				La Empresa promueve la venta y cobra los bienes y servicios disponibles
-				en la app baria, el pago se hará en dólares norteamericanos (USD
-				DOLLARS) El Usuario compra y paga haciendo uso de sus tarjetas de
-				crédito o débitos Visa o MasterCard. Con este acto declara que fueron
+				en la app baria, el pago se hará previo al envío en la moneda y monto
+				acordado entre La Empresa y El Usuario. Con este acto declara que fueron
 				leídos y aceptados todos los Términos y Condiciones que aquí se
 				describen.
 			</p>
-			<h4 style={{textAlign: 'left'}}>ACTUALIZACIONES DEL SITIO WEB. </h4>
+			<h4 style={{textAlign: 'left'}}>ACTUALIZACIONES DE LA APLICACIÓN. </h4>
 			<p>
 				La Empresa se reserva el derecho de modificar en cualquier momento y sin
-				previo aviso la presentación de la web, los contenidos, la
+				previo aviso la presentación de la aplicación, los contenidos, la
 				funcionalidad, los bienes, los servicios, los precios y la configuración
 				que pudiera estar contenida en el sitio, incrementar o dejar de ofrecer
 				algunos de los bienes y servicios y/o realizar modificaciones a este
@@ -45,7 +44,7 @@ export const Terms = () => {
 				trabajo de lunes a viernes de 8 am a 5 pm, horario de Ecuador.
 			</p>
 			<p>
-				El Usuario conoce que los precios de cada producto están incluido el
+				El Usuario conoce que en el precio final de cada compra está incluido el
 				envío hasta las instalaciones de la empresa contratada para su entrega
 				que más cerca quede a la dirección de del destinatario y este habilitado
 				para tal efecto, que no incluyen pagos de aranceles o servicios interno
@@ -71,27 +70,21 @@ export const Terms = () => {
 				la orden y asumirá los gastos de enrutamiento en caso de ser necesario.
 			</p>
 			<p>
-				Debe conocer el cliente que los combos mixtos de misceláneas su peso
-				máximo no puede sobre pasar el 3.00 kilogramos, a tal efecto están
-				distribuidas las tallas.
-			</p>
-			<p>
 				Es responsabilidad de El Usuario de informarse de las leyes aduanales
 				y/o resoluciones que regulen el ingreso de los paquetes postales y
-				mercancía con carácter no comercial al país de destino, debe conocer
-				cuáles de nuestros productos puede importar o no, La Empresa no se
+				mercancía con carácter no comercial al país de destino. La Empresa no se
 				responsabiliza por el pago de aranceles y servicios aduanales, pago de
 				servicios internos u otros impuestos, tampoco así por decomiso y/o
 				retención por violar la ley de aduanas del país de destino.
 			</p>
 			<p>
-				El Usuario ante la inconformidad por faltante o cambio en el contenido y
-				peso recibido puede formular una reclamación en un término no mayor a 24
-				horas contadas a partir de recibida la encomienda por el destinatario,
-				debe hacer por escrito al WhatsApp +593 99 568 7985. Debe aportar las
-				pruebas siguientes:
+				El Usuario ante la inconformidad por variaciones en el contenido
+				recibido y peso recibido puede formular una reclamación en un término no
+				mayor a 24 horas contadas a partir de recibida la encomienda por el
+				destinatario, debe hacer por escrito al WhatsApp +593 99 568 7985. Debe
+				aportar las pruebas siguientes:
 			</p>
-			<p>Copia de la orden de compra.</p>
+			<p>Factura de compra.</p>
 			<p>Código de seguimiento.</p>
 			<p>
 				Fotos con la totalidad del contenido recibido en una sola foto en caso
@@ -104,8 +97,8 @@ export const Terms = () => {
 			</p>
 			<p>
 				Foto del comprobante de recibo y recibo de pago del retiro de la
-				encomienda en el Correo donde coste cualquier anomalía que quiera
-				sustentar.
+				encomienda en la Oficina de Correos de Cuba donde conste cualquier
+				anomalía que quiera sustentar.
 			</p>
 			<p>
 				Se advierte, que la mercancía objeto de reclamo no puede tener uso
@@ -120,10 +113,10 @@ export const Terms = () => {
 			</p>
 			<p>
 				Si es comprobado que El Usuario tiene razón en su reclamo, por mercancía
-				defectuosa y/o mercancía no solicitada se le indicara a cuál dirección
+				defectuosa y/o mercancía no solicitada se le indicará a cuál dirección
 				deberá enviar ese paquete y una vez recibido por nuestro personal en la
-				oficina de La Habana se le reembolsara lo pagado en la factura más el
-				valor del envió si fuera el caso.
+				oficina de Sancti-Spíritus se le reembolsará lo pagado en la factura, o
+				se enviará nuevamente la mercancía a desición de El Usuario.
 			</p>
 			<p>
 				La Empresa da a conocer al El Usuario que este tipo de envíos tanto
@@ -132,20 +125,13 @@ export const Terms = () => {
 				establecer una garantía contra roturas, por lo que la empresa no
 				responderá por deterioro ocasionada por la manipulación, no se admitirá
 				reclamos por esta causa. Tampoco cabe reclamo alguno en el caso que por
-				negligencia de El Usuario se dañen los productos como son los caso, de
-				mala manipulación con es dar carga de forma incorrectas a las baterías
-				de Litio, poner aire a los neumáticos de las bicicletas en exceso, rodar
-				las bicicletas eléctrica sin aire en los neumáticos, colocar los
-				electrodomésticos a que reciban carga eléctrica que no sea la que
-				establece el manual del equipo, colocar aceites que no sea el indicado
-				en los generadores eléctricos, recomendamos leer el manual y ante dudas
-				consultar con los asesores de la página.
+				negligencia de El Usuario se dañen los productos.
 			</p>
 			<p>
-				El Usuario admite saber que una vez realizada la compra no tiene derecho
-				a reclamar reembolso de los fondos pagados por los bienes y servicios,
-				por desestimación de esta, por demoras en la entrega, la compra es
-				irreversible hasta su entrega al destinatario final.
+				Nuestros artículos salen probados de tanto de la fábrica como de La
+				Empresa, por lo cual no brindamos garantía ni nos responsabilizamos del
+				mal uso de dichos artículos. Recomendamos en todo caso leer
+				correctamente el manual del fabricante.
 			</p>
 			<p>
 				El Usuario admite saber que una vez realizada la compra no tiene derecho
@@ -163,11 +149,10 @@ export const Terms = () => {
 				tampoco reembolso por el pago de los bienes y servicios por esta causa.
 			</p>
 			<p>
-				El Usuario conoce que el servicio se presta desde la Republica de Panama
-				y Ecuador para el país de destino que las mercancías salen de cada país
-				en dependencia de sus características, también que están sujetos a leyes
-				de Exportación e Importación reguladas por las autoridades aduanales de
-				cada país.
+				El Usuario conoce que el servicio se presta desde la República del
+				Ecuador con destino a Cuba, las mercancías salen en dependencia de sus
+				características, también que están sujetos a leyes de Exportación e
+				Importación reguladas por las autoridades aduanales de cada país.
 			</p>
 			<h4>RESPONSABILIDAD DE LA EMPRESA</h4>
 			<p>
@@ -179,19 +164,17 @@ export const Terms = () => {
 			</p>
 			<p>
 				La Empresa le envía una confirmación de su compra cuando se haya
-				completado satisfactoriamente, al correo electrónico brindado en el
-				momento de hacer la orden de compra.
+				completado satisfactoriamente, al Whatsapp brindado en el momento de
+				hacer la orden de compra.
 			</p>
 			<p>
 				La Empresa una vez se haya procesado la carga en sus instalaciones
-				proporcionara en su sitio los códigos de seguimiento o identificación de
-				cada bulto, estado del mismo, El Usuario podrá obtener esta información
-				con el número de identificación del destinatario, La Empresa
-				proporcionara a solicitud de El Usuario el link de la empresas
-				contratada para que continúe su seguimiento del envió dentro del país de
-				destino una vez haya cumplido con los requisitos de internación de la
-				aduana, cuando esté listo para su entrega y hasta llegar al
-				destinatario.
+				enviará los códigos de seguimiento o identificación de cada bulto,
+				estado del mismo, La Empresa proporcionará a solicitud de El Usuario el
+				link de la empresas contratada para que continúe su seguimiento del
+				envió dentro del país de destino una vez haya cumplido con los
+				requisitos de internación de la aduana, cuando esté listo para su
+				entrega y hasta llegar al destinatario.
 			</p>
 			<p>
 				La Empresa se hace responsable de cada encomienda hasta la entrega al
@@ -199,32 +182,6 @@ export const Terms = () => {
 				situación deberá ser ratificada por la empresa bariada de hacer la
 				distribución dentro del país de destino y previo a la entrega de los
 				respaldos de El Usuario.
-			</p>
-			<h4>REMMBOLSOS</h4>
-			<p>
-				La Empresa en el caso que sea necesario efectuar un reembolso informará
-				a El Usuario al correo electrónico ofrecido y este se hará efectivo de
-				siete a quince (7 a 15) días laborables, El Usuario acepta conocer que
-				el reembolso no será por el total de la factura de compra, tendrá una
-				deducción del 10% que comprenden los gasto La Empresa en la operación.
-			</p>
-			<p>
-				Las causas de estos reembolsos serán analizadas por La Empresa, y la
-				decisión será exclusiva de la misma, pero en ningún caso podrán ser
-				cuando la carga este en tránsito y/o por las cusas siguientes:
-			</p>
-			<p>Desestimación de la compra por el cliente.</p>
-			<p>Atraso en las entregas.</p>
-			<p>Rotura o desperfectos por mala la manipulación de la mercancía.</p>
-			<p>Por decomiso de autoridades aduanales.</p>
-			<p>Por caso fortuito y/o fuerza mayor.</p>
-			<p>Por mal uso del medio de pago.</p>
-			<p>
-				La Empresa en el caso que sea necesario efectuar un reembolso informará
-				a El Usuario al correo electrónico ofrecido y este se hará efectivo de
-				siete a quince (7 a 15) días laborables, El Usuario acepta conocer que
-				el reembolso no será por el total de la factura de compra, tendrá una
-				deducción del 7% que comprenden los gasto La Empresa en la operación.
 			</p>
 			<h4>CONDICIONES GENERALES</h4>
 			<p>
@@ -247,10 +204,10 @@ export const Terms = () => {
 				Ambas partes acuerdan que cualquier litigio o controversia provenientes
 				de, o relacionados con este contrato por la compras en la app baria, así
 				como la interpretación, aplicación, ejecución y terminación de este,
-				deberán resolverse conforme a las Leyes de la República de Panamá, por
+				deberán resolverse conforme a las Leyes de la República del Ecuador, por
 				medio de Arbitraje en Derecho, previo intento de conciliación, por el
-				Centro de Conciliación y Arbitraje de la República de Panamá, conforme a
-				sus reglas de procedimiento y las Leyes panameñas que rigen esta
+				Centro de Conciliación y Arbitraje de la República del Ecuador, conforme
+				a sus reglas de procedimiento y las Leyes ecuatorianas que rigen esta
 				materia.
 			</p>
 			<p>
